b5acdaae6467a87ccd7cc540369c2850fd67ad64
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-13 00:19:55 +0200

    Implemented the create user functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,8 +1,19 @@
 import { PrismaClient } from "@prisma/client"
 
+import userHelpers from "../helpers/user.helpers"
+
 const prisma = new PrismaClient()
 
-const create = () => {}
+const create = async (req, res) => {
+    const user = userHelpers.computeFullName(req.body.user)
+    user.salt = Date.now().toString()
+    if ( userHelpers.validateUser(user) ) {
+        const newUser = await prisma.user.create({ data: user })
+        res.json({ msg: `The user ${newUser.fullName} has been created successfully`})
+    } else {
+        // TODO: Add validation handlers
+    }
+}
 const read = () => {}
 const list = () => {}
 const update = () => {}
