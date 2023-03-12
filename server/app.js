438c9816feed736968c010259cd63e9ecb26b511
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-13 00:23:25 +0200

    Added basic app configuration

--- server/app.js
+++ server/app.js
@@ -1,5 +1,24 @@
+import dotenv from "dotenv"
 import express from "express"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import compress from "compression"
+import cors from "cors"
+import helmet from "helmet"
+
+import userRouter from "./routes/user.routes"
+
+dotenv.config()
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+app.use('/api/v1/users', userRouter)
 
 export default app
