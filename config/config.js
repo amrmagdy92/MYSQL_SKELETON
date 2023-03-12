687f1c52969efef037aecaa162af05acbf14ec0c
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-12 16:07:35 +0200

    Added multiCore to the config

--- config/config.js
+++ config/config.js
@@ -3,7 +3,8 @@ const config = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "SECRET_KEY",
     mongoURI: process.env.MONGODB_URI || process.env.MONGO_HOST || `mongodb://${process.env.HOST_IP || '127.0.0.1'}:${process.env.MONGO_PORT || 27017}`,
-    jwtExpiry: 999
+    jwtExpiry: 999,
+    multiCore: true
 }
 
 export default config
