838975105473b4d0ec4f268b5565ebb226597e0f
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-12 16:19:10 +0200

    Added user controller skeleton

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+const create = () => {}
+const read = () => {}
+const list = () => {}
+const update = () => {}
+const remove = () => {}
+
+export default {
+    create,
+    read,
+    list,
+    update,
+    remove
+}
