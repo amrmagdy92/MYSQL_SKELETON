c1e908452aa08ebde1478cefc5884c51d1979edc
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-13 14:45:06 +0200

    Implemented create function

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,21 @@
-const create = () => {}
+import userSchema from "../models/user.model"
+import userHelpers from "../helpers/user.helpers"
+
+const create = (req, res) => {
+    var user = req.body.user
+    user.salt = userHelpers.makeSalt()
+    user = userHelpers.computeFullName(user)
+    user.hashedPassword = userHelpers.encryptPassword(user.password, user.salt)
+    // res.send(user)
+    userSchema
+        .create(user)
+        .then(() => {
+            res.json({msg: `The user ${req.body.user.firstName} ${req.body.user.lastName} was created successfully`})
+        })
+        .catch((err) => {
+            res.json({ msg: err })
+        })
+}
 const read = () => {}
 const list = () => {}
 const update = () => {}
