cbb6b18c296596c002e66cbb2e2cd60e5cd7ab9a
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-13 15:42:24 +0200

    Added getErrorMessage and modified the exports accordingly

--- server/helpers/db.helpers.js
+++ server/helpers/db.helpers.js
@@ -15,4 +15,17 @@ sequelize
     .then( () => { console.log('Database is now in sync...') })
     .catch( (err) => { console.log(`Synchornization was not done and the following error was caught:\n${err}`) })
 
-export default sequelize
+const getErrorMessage = (receivedError) => {
+    const errorMessages = {}
+    for (let i = 0; i < receivedError.errors.length; i++) {
+        path = receivedError.errors[i].path
+        msg = receivedError.errors[i].message
+        errorMessages.path = msg
+    }
+    return errorMessages
+}
+
+export {
+    sequelize,
+    getErrorMessage
+}
