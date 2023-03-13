d4e184c3b618f10110ad3cc17a27e9676d99bc27
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-13 14:42:55 +0200

    Reworked instantiation

--- server/helpers/db.init.js
+++ server/helpers/db.init.js
@@ -1,8 +1,14 @@
 import { Sequelize } from "sequelize"
 
-const sequelize = new Sequelize({
-    dialect: "mysql"
-})
+const sequelize = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        host: process.env.DATABASE_HOST,
+        dialect: "mysql"
+    }
+)
 
 sequelize
     .sync()
