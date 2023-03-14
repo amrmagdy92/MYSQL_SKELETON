8275c7ec84ab17a6181465dfd9c2b689c7145de6
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-14 10:04:02 +0200

    removed unnecessary configuration and made it all dependant on the .env file

--- server/config/server.conf.js
+++ server/config/server.conf.js
@@ -1,10 +1,8 @@
 const config = {
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
+    port: process.env.SERVER_PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "SECRET_KEY",
-    mongoURI: process.env.MONGODB_URI || process.env.MONGO_HOST || `mongodb://${process.env.HOST_IP || '127.0.0.1'}:${process.env.MONGO_PORT || 27017}`,
-    jwtExpiry: 999,
-    multiCore: true
+    jwtExpiry: process.env.JWT_EXPIRY || 999
 }
 
 export default config
