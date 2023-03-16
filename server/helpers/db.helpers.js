03abf6c5cdeda1f367656cea5df9b4e0ccfa4ba3
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-16 09:58:08 +0200

    Added alter option to sequelize sync function to ensure database synchronization is done properly when models change

--- server/helpers/db.helpers.js
+++ server/helpers/db.helpers.js
@@ -14,7 +14,7 @@ const sequelize = new Sequelize(
 )
 
 sequelize
-    .sync()
+    .sync({ alter: true })
     .then( () => { console.log('Database is now in sync...') })
     .catch( (err) => { console.log(`Synchornization was not done and the following error was caught:\n${err}`) })
 
