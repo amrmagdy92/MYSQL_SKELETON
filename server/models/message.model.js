1da1a72fe150422e1bb3db1bd6e8aa491cdc409e
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-16 16:38:58 +0200

    Added base model for messages

---
+++ server/models/message.model.js
@@ -0,0 +1,40 @@
+import { DataTypes, Sequelize } from "sequelize"
+
+import { sequelize } from "../helpers/db.helpers"
+
+const messageSchema = sequelize.define('message', {
+    sender: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            is: {
+                args: /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4,})$/,
+                msg: "The provided phone is invalid"
+            }
+        }
+    },
+    recepient: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            is: {
+                args: /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4,})$/,
+                msg: "The provided phone is invalid"
+            }
+        }
+    },
+    messageType: {
+        type: DataTypes.ENUM("GIF", "TEXT", "VIDEO", "PIC"),
+        allowNull: false
+        // TODO: Add reference to message content to retrieve it from the specified table
+    },
+    sentDatetime: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.now
+    }
+}, {
+    paranoid: true
+})
+
+export default messageSchema
