99e7b7b296fb3e5eeaca67348cb908aec408d2d6
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-17 23:22:28 +0200

    Implemented create function

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,7 +1,25 @@
 import messageSchema from "../models/message.model"
 import { getErrorMessage } from "../helpers/db.helpers"
 
-const create = () => {}
+const create = (req, res) => {
+    var msg = {}
+    var msgValidator = {}
+
+    req.body.message.sender? msg.sender = req.body.message.sender : msgValidator.sender = "A sender is required"
+    req.body.message.recepient? msg.recepient = req.body.message.recepient : msgValidator.recepient = "A recepient is required"
+    req.body.message.messageData? msg.messageData = req.body.message.messageData : null
+    req.body.message.messageText? msg.messageText = req.body.message.messageText : null
+    req.body.message.sentDatetime? msg.sentDatetime = req.body.message.sentDatetime : msgValidator.sentDatetime = "Sent time is required"
+
+    if (msgValidator.keys.length > 0) {
+        res.json(msgValidator)
+    } else {
+        messageSchema
+            .create(msg)
+            .then( () => { res.json({ msg: "message sent successfully"}) } )
+            .catch( (err) =>  res.json(getErrorMessage(err)) )
+    }
+}
 const list = () => {}
 const read = () => {}
 const update = () => {}
