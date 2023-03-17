f2aa6f7c08c39151286541bdc9fea2de62b2d081
Amr Magdy <amr.magdy.ahmed92@gmail.com>
2023-03-17 23:22:08 +0200

    Added base file for message controller

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,16 @@
+import messageSchema from "../models/message.model"
+import { getErrorMessage } from "../helpers/db.helpers"
+
+const create = () => {}
+const list = () => {}
+const read = () => {}
+const update = () => {}
+const remove = () => {}
+
+export {
+    create,
+    list,
+    read,
+    update,
+    remove
+}
